ea1c2ac31598ab82b2020c16e6eaadce73e7ba9c
David Lee <Dai180293@gmail.com>
2025-12-02 07:22:33 +0000

    removed trigger

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { SidebarTrigger } from "@/components/ui/sidebar";
-
 export default function Header() {
     return (
-        <header className="flex justify-between items-center px-2 md:px-5 z-50">
-            <SidebarTrigger className="w-9 h-9 md:hidden cursor-pointer" />
+        <header className="flex justify-between items-center px-2 md:px-5 z-50 border-b border-border">
             <h1>hello</h1>
         </header>
     );
