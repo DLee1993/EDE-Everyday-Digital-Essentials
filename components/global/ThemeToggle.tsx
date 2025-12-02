64f9a7990b16ffbae4769e73bea8065607ab1a6f
David Lee <Dai180293@gmail.com>
2025-12-02 07:22:15 +0000

    fixed movement on mount

--- components/global/ThemeToggle.tsx
+++ components/global/ThemeToggle.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
+import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 
-const ChangeTheme = ({ open, isMobile }: { open: boolean; isMobile: boolean }) => {
+const ChangeTheme = () => {
     const [hasMounted, setHasMounted] = useState(false); // <-- add this
     const { theme, setTheme } = useTheme();
 
@@ -26,37 +26,38 @@ const ChangeTheme = ({ open, isMobile }: { open: boolean; isMobile: boolean }) =
         <SidebarMenuItem onClick={toggleTheme}>
             <SidebarMenuButton
                 asChild
-                tooltip="Change theme"
-                className="min-h-9 min-w-9 cursor-pointer group-data-[collapsible=icon]:p-0!"
+                className="min-h-9 min-w-9 cursor-pointer"
             >
                 {hasMounted ? (
-                    <div className="w-full px-0">
-                        <div className="relative min-h-9 min-w-9">
+                    <div className="px-0">
+                        <div className="relative min-h-8 min-w-8">
                             <MoonIcon
                                 size={16}
-                                className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-500 ${
-                                    theme === "dark" || theme === "system" ? "scale-100" : "scale-0"
+                                className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  ${
+                                    theme === "dark" || theme === "system" ? "block" : "hidden"
                                 }`}
                             />
                             <SunIcon
                                 size={18}
-                                className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-500 ${
-                                    theme === "light" ? "scale-100" : "scale-0"
+                                className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  ${
+                                    theme === "light" ? "block" : "hidden"
                                 }`}
                             />
                         </div>
 
-                        <p
-                            className={`flex items-center w-full min-w-32 ${
-                                !open && !isMobile && "opacity-0"
-                            }`}
-                        >
+                        <p className="flex items-center w-full min-w-32">
                             {theme === "dark" ? "Dark mode" : "Light mode"}
                         </p>
                     </div>
                 ) : (
-                    <div className="w-full min-w-32 ml-2.5">
-                        <SunMoonIcon size={16} />
+                    <div className="px-0">
+                        <div className="relative min-h-8 min-w-8">
+                            <MoonIcon
+                                size={16}
+                                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                            />
+                        </div>
+                        <p className="flex items-center w-full min-w-32">Dark mode</p>
                     </div>
                 )}
             </SidebarMenuButton>
