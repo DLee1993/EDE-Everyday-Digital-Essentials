e0c30802c1f76d1d45fa55bfc6985566aeb4bf3e
David Lee <Dai180293@gmail.com>
2025-12-02 07:21:55 +0000

    updated styling

--- components/global/PageIdentifier.tsx
+++ components/global/PageIdentifier.tsx
@@ -1,6 +1,6 @@
 export const PageIdentifier = () => {
     return (
-        <div className="relative size-1.5 rounded-full ml-auto">
+        <div className="relative size-1 rounded-full ml-auto">
             <div className="absolute top-0 left-0 w-full h-full rounded-full bg-accent"></div>
         </div>
     );
