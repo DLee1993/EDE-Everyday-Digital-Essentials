9cbe35b1778d908ce6ca8e2b65454a8aa3629867
David Lee <Dai180293@gmail.com>
2025-12-02 07:24:03 +0000

    created a sidebar on tablet/desktop and a drawer menu for mobile

--- components/global/App-Sidebar.tsx
+++ components/global/App-Sidebar.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { sidebarItems } from "@/components/global/app-sidebar-item-list";
 import ChangeTheme from "@/components/global/ThemeToggle";
-import { Home, FolderGit2, Settings } from "lucide-react";
+import { Home, FolderGit2, Settings, ChevronRight, XIcon } from "lucide-react";
 import {
     Sidebar,
     SidebarContent,
@@ -15,32 +15,80 @@ import {
     SidebarMenuItem,
     SidebarSeparator,
     useSidebar,
-    SidebarTrigger,
     SidebarGroupLabel,
 } from "@/components/ui/sidebar";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
 import { PageIdentifier } from "@/components/global/PageIdentifier";
 
 export function AppSidebar() {
     const pathname = usePathname();
-    console.log(pathname)
-    const { open, isMobile, setOpen, setOpenMobile } = useSidebar();
+    const { isMobile } = useSidebar();
 
-    const toggleMenu = (bool: boolean) => {
-        setOpen(bool);
-        setOpenMobile(bool);
-    };
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-foreground/10 px-6 py-1.5 rounded-md cursor-pointer">
+                    Menu
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader className="flex flex-row justify-between items-center">
+                        <DrawerTitle>Select a tool</DrawerTitle>
+                        <DrawerClose className="w-fit cursor-pointer">
+                            <XIcon />
+                        </DrawerClose>
+                        <DrawerDescription className="sr-only">
+                            Select a tool from the list below
+                        </DrawerDescription>
+                    </DrawerHeader>
+                    <div className="space-y-4 min-h-72 overflow-scroll p-2">
+                        {sidebarItems.map((category) => (
+                            <div key={category.title} className="bg-foreground/5 rounded-md p-4">
+                                <h2 className="text-sm font-semibold mb-3 text-accent">
+                                    {category.title}
+                                </h2>
+                                <ul className="space-y-2">
+                                    {category.items.map((tool) => (
+                                        <li key={tool.url}>
+                                            <Link
+                                                href={tool.url}
+                                                className="flex items-center justify-between gap-2 text-sm p-2 rounded hover:bg-foreground/20"
+                                            >
+                                                <div className="flex items-center gap-2">
+                                                    <tool.icon className="w-4 h-4" />
+                                                    <span className="whitespace-nowrap">
+                                                        {tool.title}
+                                                    </span>
+                                                </div>
+                                                <ChevronRight className="w-4 h-4 text-muted" />
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* <DrawerFooter>
+                        <DrawerClose>Cancel</DrawerClose>
+                    </DrawerFooter> */}
+                </DrawerContent>
+            </Drawer>
+        );
+    }
 
     return (
-        <Sidebar collapsible="icon" variant="sidebar">
+        <Sidebar collapsible="none" className="h-screen border-r border-border">
             <SidebarHeader className="overflow-hidden px-3 py-2.5 hidden md:block">
                 <div className="relative hidden md:block">
-                    <SidebarTrigger className="absolute top-1/2 -translate-y-1/2 -right-[5.9px] min-w-9 min-h-9 z-50 cursor-pointer hover:bg-foreground/15 hover:text-accent" />
-
-                    <p
-                        className={`flex flex-col min-w-52 transition-opacity duration-200 ease-linear text-md font-semibold ${
-                            !open && "opacity-0"
-                        }`}
-                    >
+                    <p className="flex flex-col min-w-52 transition-opacity duration-200 ease-linear text-md font-semibold">
                         EDE
                         <span className="text-xs text-muted-foreground font-normal">
                             Your Everyday Digital Essentials.
@@ -48,21 +96,15 @@ export function AppSidebar() {
                     </p>
                 </div>
             </SidebarHeader>
-            <SidebarSeparator className="mx-0" />
-            <SidebarContent
-                className={`overflow-x-hidden ${
-                    open ? "overflow-y-auto" : "overflow-y-auto! hiddenScrollbar"
-                }`}
-            >
+            <SidebarSeparator className="mx-0 h-[0.5px]!" />
+            <SidebarContent>
                 <SidebarGroup key={`Dashboard menu group`}>
                     <SidebarMenu>
                         <SidebarMenuItem>
                             <SidebarMenuButton
                                 asChild
-                                tooltip="Home"
                                 isActive={pathname === "/" ? true : false}
                                 className="min-h-9 min-w-9"
-                                onClick={() => toggleMenu(false)}
                             >
                                 <Link
                                     href="/"
@@ -70,38 +112,21 @@ export function AppSidebar() {
                                 >
                                     <Home
                                         size={15}
-                                        className={`ml-0.5 ${
+                                        className={`${
                                             pathname === "/" ? "text-accent" : "text-foreground"
                                         }`}
                                     />
-                                    <p
-                                        className={`mx-2 min-w-32 transition-opacity duration-200 ease-linear ${
-                                            !open && !isMobile && "opacity-0"
-                                        }`}
-                                    >
-                                        Home
-                                    </p>
+                                    <p className="mx-2 min-w-32">Home</p>
                                     {pathname === "/" && <PageIdentifier />}
                                 </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
 
                         <SidebarMenuItem>
-                            <SidebarMenuButton
-                                asChild
-                                tooltip="Request a tool"
-                                className="min-h-9 min-w-9"
-                                onClick={() => toggleMenu(false)}
-                            >
+                            <SidebarMenuButton asChild className="min-h-9 min-w-9">
                                 <Link href="https://github.com/DLee1993/toolbox" target="_blank">
-                                    <FolderGit2 size={15} className="ml-0.5" />
-                                    <p
-                                        className={`mx-2 min-w-32 transition-opacity duration-200 ease-linear ${
-                                            !open && !isMobile && "opacity-0"
-                                        }`}
-                                    >
-                                        Request a tool
-                                    </p>
+                                    <FolderGit2 size={15} />
+                                    <p className="mx-2 min-w-32">Request a tool</p>
                                 </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
@@ -109,42 +134,29 @@ export function AppSidebar() {
                         <SidebarMenuItem>
                             <SidebarMenuButton
                                 asChild
-                                tooltip="Settings"
                                 isActive={pathname === "/settings" ? true : false}
                                 className="min-h-9 min-w-9"
-                                onClick={() => toggleMenu(false)}
                             >
                                 <Link href="/settings">
                                     <Settings
                                         size={15}
-                                        className={`ml-0.5 ${
+                                        className={`${
                                             pathname === "/settings"
                                                 ? "text-accent"
                                                 : "text-foreground"
                                         }`}
                                     />
-                                    <p
-                                        className={`mx-2 min-w-32 transition-opacity duration-200 ease-linear ${
-                                            !open && !isMobile && "opacity-0"
-                                        }`}
-                                    >
-                                        Settings
-                                    </p>
+                                    <p className="mx-2 min-w-32">Settings</p>
                                     {pathname === "/settings" && <PageIdentifier />}
                                 </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
-                        <ChangeTheme open={open} isMobile={isMobile} />
+                        <ChangeTheme />
                     </SidebarMenu>
                 </SidebarGroup>
-
-                <SidebarSeparator className="bg-border" />
-
+                <SidebarSeparator className="mx-0" />
                 {sidebarItems.map((group) => (
-                    <SidebarGroup
-                        key={`${group.title} menu group`}
-                        className={`${open ? "py-2" : "py-0"} mb-5 md:mb-0`}
-                    >
+                    <SidebarGroup key={`${group.title} menu group`} className="py-2 mb-5 md:mb-0">
                         <SidebarGroupLabel
                             className={`pointer-events-none text-accent text-[13px] ${
                                 isMobile ? "h-8" : "h-5"
@@ -157,31 +169,23 @@ export function AppSidebar() {
                                 <SidebarMenuItem key={`menu-item-${i}`}>
                                     <SidebarMenuButton
                                         asChild
-                                        tooltip={item.title}
                                         isActive={pathname === item.url ? true : false}
                                         className="min-h-9 min-w-9"
-                                        onClick={() => toggleMenu(false)}
                                     >
                                         <Link
                                             href={item.url}
-                                            className="data-[active=true]:bg-foreground data-[active=true]:text-background space-x-2"
+                                            className="data-[active=true]:bg-foreground data-[active=true]:text-background space-x-1"
                                         >
                                             {item.icon && (
                                                 <item.icon
-                                                    className={`ml-0.5 ${
+                                                    className={`${
                                                         pathname === item.url
                                                             ? "text-primary"
                                                             : "text-foreground"
                                                     }`}
                                                 />
                                             )}
-                                            <p
-                                                className={`min-w-32 transition-opacity duration-200 ease-linear whitespace-nowrap ${
-                                                    !open && !isMobile && "opacity-0"
-                                                }`}
-                                            >
-                                                {item.title}
-                                            </p>
+                                            <p className="min-w-32">{item.title}</p>
                                             {pathname === item.url && <PageIdentifier />}
                                         </Link>
                                     </SidebarMenuButton>
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -22,7 +22,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
-const SIDEBAR_WIDTH = "16rem";
+const SIDEBAR_WIDTH = "18rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "3.2rem";
 const SIDEBAR_KEYBOARD_SHORTCUT = "b";
@@ -356,7 +356,7 @@ function SidebarContent({ className, ...props }: React.ComponentProps<"div">) {
             data-slot="sidebar-content"
             data-sidebar="content"
             className={cn(
-                "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+                "flex min-h-0 flex-1 flex-col gap-1 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
                 className
             )}
             {...props}
@@ -453,11 +453,11 @@ function SidebarMenuItem({ className, ...props }: React.ComponentProps<"li">) {
 }
 
 const sidebarMenuButtonVariants = cva(
-    "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-foreground/15 data-[active=true]:font-medium data-[active=true]:text-sidebar-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
+    "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-foreground/6 data-[active=true]:font-medium data-[active=true]:text-sidebar-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
     {
         variants: {
             variant: {
-                default: "hover:bg-foreground/10",
+                default: "hover:bg-foreground/6",
                 outline:
                     "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
             },
