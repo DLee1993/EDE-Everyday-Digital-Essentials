0e3f7f89aea1c4ceee52b2a4349bfa90fb5ae04b
David Lee <Dai180293@gmail.com>
2025-12-03 11:04:14 +0000

    added mobile menu trigger

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function Header() {
     return (
         <header className="flex justify-between items-center px-2 md:px-5 z-50 border-b border-border">
-            <h1>hello</h1>
+            <div className="flex justify-center items-center gap-5">
+                <SidebarTrigger variant="secondary" className="lg:hidden cursor-pointer" />
+                <h1>hello</h1>
+            </div>
         </header>
     );
 }
