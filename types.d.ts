f29a3a56b8678efd30fb5f364b0d95612777bee2
David Lee <Dai180293@gmail.com>
2025-12-08 22:27:39 +0000

    added shadcn components

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,44 @@ type Greeting = {
     message: string;
     icon: React.ReactNode;
 };
+
+type LinkInBioValues = {
+    name?: string;
+    email?: string;
+    description?: string;
+    photo?: string;
+    portfolio?: string;
+    facebook?: string;
+    twitter?: string;
+    linkedin?: string;
+    instagram?: string;
+    github?: string;
+    telegram?: string;
+    youtube?: string;
+    whatsApp?: string;
+    [key: string]: string;
+};
+
+type NotepadNoteValues = {
+    title: string;
+    content?: string;
+    category?: string;
+    completedBy: Date | undefined;
+    id: string;
+    createdAt: string;
+    completed: boolean;
+};
+
+type Action = {
+    file: File;
+    file_name: string;
+    file_size: number;
+    from: string;
+    to: string | null;
+    file_type: string;
+    is_converting?: boolean;
+    is_converted?: boolean;
+    is_error?: boolean;
+    url?: string;
+    output?: string;
+};
