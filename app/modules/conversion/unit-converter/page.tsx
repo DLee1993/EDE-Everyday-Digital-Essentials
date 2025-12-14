42b2098a2f74a69f92276ee4271c138608412b1d
David Lee <Dai180293@gmail.com>
2025-12-14 12:55:04 +0000

    added the unit converer component

---
+++ app/modules/conversion/unit-converter/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useState } from "react";
+import { Unit } from "convert-units";
+import { ConvertUnits } from "@/lib/unit-converter/convertUnits";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SelectUnit } from "@/components/unit-converter/SelectUnit";
+import { CircleAlert, CircleX } from "lucide-react";
+
+export default function UnitConverter() {
+    const [amount, setAmount] = useState("");
+    const [selectedValue, setSelectedValue] = useState<{ from: Unit | ""; to: Unit | "" }>({
+        from: "",
+        to: "",
+    });
+    const [result, setResult] = useState<number | null>(null);
+    const [error, setError] = useState<string>("");
+
+    const ClearUnits = () => {
+        setSelectedValue({ from: "", to: "" });
+        setAmount("");
+        setResult(null);
+        setError("");
+    };
+
+    return (
+        <section className="relative space-y-10 md:space-y-20 max-w-3xl">
+            <h2 className="w-full">Select a Unit to convert</h2>
+            <section className="w-full flex flex-col md:flex-row gap-10">
+                <div className="w-full flex flex-col gap-2">
+                    <h2 className="text-sm font-semibold text-muted-foreground">From</h2>
+                    <div className="flex gap-2">
+                        <SelectUnit
+                            type="from"
+                            selectedValue={selectedValue}
+                            setSelectedValue={setSelectedValue}
+                        />
+                        <Input
+                            value={amount}
+                            id="amount"
+                            onChange={(e) => setAmount(e.target.value)}
+                            className="text-center"
+                            placeholder="Amount"
+                            type="number"
+                        />
+                    </div>
+                </div>
+                <div className="w-full flex flex-col gap-2">
+                    <h2 className="text-sm font-semibold text-muted-foreground">To</h2>
+                    <div className="flex gap-2">
+                        <SelectUnit
+                            type="to"
+                            selectedValue={selectedValue}
+                            setSelectedValue={setSelectedValue}
+                        />
+                        <Input
+                            readOnly
+                            id="result"
+                            value={result !== null ? Number(result).toLocaleString() : ""}
+                            className="text-center"
+                        />
+                    </div>
+                </div>
+                <p className="min-h-5 text-red-600 text-sm font-medium">
+                    {error && (
+                        <span className="flex items-center gap-2">
+                            <CircleAlert size={15} />
+                            {error}
+                        </span>
+                    )}
+                </p>
+            </section>
+            <div className="w-full flex gap-5">
+                <Button
+                    variant="secondary"
+                    className="text-sm"
+                    onClick={() => ClearUnits()}
+                    disabled={!selectedValue.from}
+                >
+                    Reset
+                    <CircleX />
+                </Button>
+                <Button
+                    onClick={() => {
+                        ConvertUnits({
+                            amount,
+                            from: selectedValue.from,
+                            to: selectedValue.to,
+                            setResult,
+                            setError,
+                        });
+                    }}
+                >
+                    Convert
+                </Button>
+            </div>
+        </section>
+    );
+}
---
+++ components/unit-converter/SelectUnit.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import * as React from "react";
+import { MeasurementTypes } from "@/lib/unit-converter/convertUnits";
+import { Unit } from "convert-units";
+
+import { useMediaQuery } from "@/lib/global/use-media-query";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+import {
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
+
+type Props = {
+    type: "from" | "to";
+    selectedValue: {
+        from: Unit | "";
+        to: Unit | "";
+    };
+    setSelectedValue: React.Dispatch<
+        React.SetStateAction<{
+            from: Unit | "";
+            to: Unit | "";
+        }>
+    >;
+};
+
+export function SelectUnit({ type, selectedValue, setSelectedValue }: Props) {
+    const [open, setOpen] = React.useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+        return (
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="secondary" className="flex justify-between items-center">
+                        {selectedValue[type] ? selectedValue[type] : `Select unit`}
+                        <ChevronDown className={`${open ? "rotate-180" : "rotate-0"}`} />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] h-60 p-0" align="start">
+                    <UnitList setOpen={setOpen} setSelectedUnit={setSelectedValue} type={type} />
+                </PopoverContent>
+            </Popover>
+        );
+    }
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <Button variant="outline" className="sm:w-[150px]">
+                    {selectedValue[type] ? selectedValue[type] : `Convert ${type}`}
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="mt-4 border-t">
+                    <DrawerHeader>
+                        <DrawerTitle>Select a unit</DrawerTitle>
+                        <DrawerDescription>Choose the unit you want to use.</DrawerDescription>
+                    </DrawerHeader>
+                    <UnitList setOpen={setOpen} setSelectedUnit={setSelectedValue} type={type} />
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+}
+
+function UnitList({
+    setOpen,
+    setSelectedUnit,
+    type,
+}: {
+    setOpen: (open: boolean) => void;
+    setSelectedUnit: React.Dispatch<React.SetStateAction<{ from: Unit | ""; to: Unit | "" }>>;
+    type: string;
+}) {
+    return (
+        <Command>
+            <CommandInput placeholder="Filter units..." autoFocus />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                {MeasurementTypes.map((mType) => (
+                    <CommandGroup key={mType.measurement}>
+                        <h4 className="text-sm font-semibold p-2 capitalize">
+                            {mType.measurement}
+                        </h4>
+                        {mType.units.map((unit, i) => (
+                            <CommandItem
+                                key={i}
+                                className="cursor-pointer ml-1 hover:bg-popover-foreground/10!"
+                                value={unit}
+                                onSelect={(value) => {
+                                    setSelectedUnit((prev) => ({ ...prev, [type]: value }));
+                                    setOpen(false);
+                                }}
+                            >
+                                {unit}
+                            </CommandItem>
+                        ))}
+                        {/* <CommandSeparator /> */}
+                    </CommandGroup>
+                ))}
+            </CommandList>
+        </Command>
+    );
+}
