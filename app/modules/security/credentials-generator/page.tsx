18a8f9071c7ff323b654da1b3596d00e5245b08b
David Lee <Dai180293@gmail.com>
2025-12-14 18:37:35 +0000

    added credentials generator page

---
+++ app/modules/security/credentials-generator/page.tsx
@@ -0,0 +1,22 @@
+import Password from "@/components/credentials-generator/Password";
+import Pin from "@/components/credentials-generator/Pin";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function CredentialsGenerator() {
+    return (
+        <div className="flex w-full max-w-2xl flex-col gap-6">
+            <Tabs defaultValue="password">
+                <TabsList className="w-52 mb-10">
+                    <TabsTrigger value="password" className="cursor-pointer">Password</TabsTrigger>
+                    <TabsTrigger value="pin" className="cursor-pointer">Pin</TabsTrigger>
+                </TabsList>
+                <TabsContent value="password">
+                    <Password />
+                </TabsContent>
+                <TabsContent value="pin">
+                    <Pin />
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
+}
