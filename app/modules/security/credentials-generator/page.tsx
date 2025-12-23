32598adb81a4045aa350c5345c117a334860157f
David Lee <Dai180293@gmail.com>
2025-12-23 13:55:30 +0000

    added how to use component

--- app/modules/security/credentials-generator/page.tsx
+++ app/modules/security/credentials-generator/page.tsx
@@ -1,14 +1,20 @@
 import Password from "@/components/credentials-generator/Password";
 import Pin from "@/components/credentials-generator/Pin";
+import HowToUse from "@/components/global/How-to-use";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function CredentialsGenerator() {
     return (
         <div className="flex w-full max-w-2xl flex-col gap-6">
+            <HowToUse description="Select either a Pin or Password, select the desired length and click generate." />
             <Tabs defaultValue="password">
                 <TabsList className="w-52 mb-10">
-                    <TabsTrigger value="password" className="cursor-pointer">Password</TabsTrigger>
-                    <TabsTrigger value="pin" className="cursor-pointer">Pin</TabsTrigger>
+                    <TabsTrigger value="password" className="cursor-pointer">
+                        Password
+                    </TabsTrigger>
+                    <TabsTrigger value="pin" className="cursor-pointer">
+                        Pin
+                    </TabsTrigger>
                 </TabsList>
                 <TabsContent value="password">
                     <Password />
