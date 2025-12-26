6e5f475c7212408d8100d996576105a2699e332d
David Lee <Dai180293@gmail.com>
2025-12-26 06:54:21 +0000

    added converter functions

---
+++ hooks/unit-converter/useUnitConverter.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import { Unit } from "convert-units";
+import { ConvertUnits } from "@/lib/unit-converter/convertUnits";
+
+export function useUnitConverter() {
+    const [amount, setAmount] = useState("");
+    const [selectedValue, setSelectedValue] = useState<{ from: Unit | ""; to: Unit | "" }>({
+        from: "",
+        to: "",
+    });
+    const [result, setResult] = useState<number | null>(null);
+    const [error, setError] = useState("");
+
+    const convert = () => {
+        ConvertUnits({
+            amount,
+            from: selectedValue.from,
+            to: selectedValue.to,
+            setResult,
+            setError,
+        });
+    };
+
+    const reset = () => {
+        setSelectedValue({ from: "", to: "" });
+        setAmount("");
+        setResult(null);
+        setError("");
+    };
+
+    return {
+        amount,
+        setAmount,
+        selectedValue,
+        setSelectedValue,
+        result,
+        error,
+        convert,
+        reset,
+    };
+}
