5ac3cfc86815a60037c5dd8594e2139a69f6be4a
David Lee <Dai180293@gmail.com>
2025-12-26 02:23:30 +0000

    moved to hooks/global

--- lib/global/use-mounted.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react-hooks/set-state-in-effect */
-import { useEffect, useState } from "react";
-
-export function useMounted() {
-    const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), []);
-    return mounted;
-}
