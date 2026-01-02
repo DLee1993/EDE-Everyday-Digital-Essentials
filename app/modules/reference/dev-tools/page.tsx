e8d01ab482873849544b787897551dff4a350d3f
David Lee <Dai180293@gmail.com>
2026-01-02 23:11:11 +0000

    added reference category routes

---
+++ app/modules/reference/dev-tools/page.tsx
@@ -0,0 +1,5 @@
+export default function DevTools() {
+  return (
+    <div>DevTools</div>
+  )
+}
---
+++ app/modules/reference/web-development/page.tsx
@@ -0,0 +1,5 @@
+export default function WebDeveloper() {
+  return (
+    <div>WebDeveloper</div>
+  )
+}
